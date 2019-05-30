88188c7c174df94fc83a094fe24455cdffe9c932
filedesless <filedesless@gmail.com>
2019-05-30 09:36:11 -0400

    caching compl

--- zsh/oh-my-zsh/plugins/oc/oc.plugin.zsh
+++ zsh/oh-my-zsh/plugins/oc/oc.plugin.zsh
@@ -3,5 +3,13 @@
 # Author: https://github.com/kevinkirkup
 
 if [ $commands[oc] ]; then
-  source <(oc completion zsh)
+    __OC_COMPLETION_FILE="${ZSH_CACHE_DIR}/oc_completion"
+
+    if [[ ! -f $__OC_COMPLETION_FILE ]]; then
+	oc completion zsh >! $__OC_COMPLETION_FILE
+    fi
+
+    [[ -f $__OC_COMPLETION_FILE ]] && source $__OC_COMPLETION_FILE
+
+    unset __OC_COMPLETION_FILE
 fi
